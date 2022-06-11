a2aeccf826bf06839e9512e2718d0833e1f62390
Rodrigowsky <rmelo.res@gmail.com>
2022-06-11 16:34:29 +0000

    implemented token based auth and restricted mutations to auth users only

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-require('dotenv').config()
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { ApolloServer, gql } = require("apollo-server");
-const { v4: uuidv4 } = require('uuid');
-const mongoose = require("mongoose")
-const Book = require('./models/books')
-const Author = require('./models/authors')
+const { v4: uuidv4 } = require("uuid");
+const mongoose = require("mongoose");
+const Book = require("./models/books");
+const Author = require("./models/authors");
+const User = require("./models/user")
 
 // let authors = [
 //   {
@@ -93,13 +95,17 @@ const Author = require('./models/authors')
 //   },
 // ];
 
-mongoose.connect(process.env.TEST_MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(process.env.TEST_MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log('connected to MongoDB')
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
+    console.log("error connection to MongoDB:", error.message);
+  });
 
 const typeDefs = gql`
   type Book {
@@ -135,19 +141,10 @@ const typeDefs = gql`
       published: Int!
       author: String!
       genres: [String!]!
-    ): Book,
-    editAuthor(
-      name: String!
-      year: Int!
-    ): Author,
-    createUser(
-      username: String!
-      favouriteGenre: String!
-    ): User,
-    login(
-      username: String!
-      password: String!
-    ): Token
+    ): Book
+    editAuthor(name: String!, year: Int!): Author
+    createUser(username: String!, favouriteGenre: String!): User
+    login(username: String!, password: String!): Token
   }
 `;
 
@@ -158,16 +155,16 @@ const resolvers = {
       const result = await Book.find({});
       return result
         .map((at) => at.author)
-        .filter((item, i, ar) => ar.indexOf(item) === i).length
+        .filter((item, i, ar) => ar.indexOf(item) === i).length;
     },
     allBooks: async (root, args) => {
       if (args.author) {
-        let findAuthor = await Author.find({ name: args.author }); 
-        return await Book.find({ author: findAuthor[0]._id}).exec();
+        let findAuthor = await Author.find({ name: args.author });
+        return await Book.find({ author: findAuthor[0]._id }).exec();
       }
       if (args.genre) {
-        const result = await Book.find({})
-          result.filter((p) => {
+        const result = await Book.find({});
+        result.filter((p) => {
           if (p.genres.find((a) => a === args.genre)) {
             console.log(p);
             return p;
@@ -180,42 +177,100 @@ const resolvers = {
       const result = await Author.find({});
       return result;
     },
+    me: (root, args, context) => {
+      
+      // it just returns the logged -in user it receives in the currentUser field of the third parameter of the resolver, context.It's worth noting that if there is no logged-in user, i.e. there is no valid token in the header attached to the request, the query returns null
+      
+      return context.currentUser
+    }
   },
   Author: {
     author: (root) => root.name,
-    born : (root) => root.born,
+    born: (root) => root.born,
     bookCount: async (root) => {
-      const result = await Book.find({})
+      const result = await Book.find({});
       return result.filter((p) => p.author === root.name).length;
     },
   },
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, context) => {
 
-      let findAuthor = await Author.find({ name: args.author }) 
+      const currentUser = context.currentUser;
+      if (!currentUser) {
+        throw new AuthenticationError("not authenticated")
+      }
+
+      let findAuthor = await Author.find({ name: args.author });
       if (findAuthor.length === 0) {
         auth = new Author({ name: args.author });
         await auth.save();
         findAuthor.push(auth);
-        
       }
-      
-      const book = new Book({ title: args.title, published: args.published, author: findAuthor[0], genres: args.genres });
-      await book.save();
+
+      const book = new Book({
+        title: args.title,
+        published: args.published,
+        author: findAuthor[0],
+        genres: args.genres,
+      });
+      return await book.save();
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, context) => {
+
+      const currentUser = context.currentUser;
+      if (!currentUser) {
+        throw new AuthenticationError("not authenticated")
+      }
+
       const filter = { name: args.name };
       const update = { born: args.year };
-      
-       return await Author.findOneAndUpdate(filter, update);
-      
-    }
-  }
+
+      return await Author.findOneAndUpdate(filter, update);
+    },
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username });
+
+      return user.save().catch((error) => {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        });
+      });
+    },
+    login: async (root, args) => {
+      // the login mutation checks if the username/password(hardcoded) pair is valid. And if it is indeed valid, it returns a jwt token
+
+      const user = await User.findOne({ username: args.username });
+
+      if (!user || args.password !== "secret") {
+        throw new UserInputError("wrong credentials");
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) };
+    },
+  },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+
+    // The object returned by context is given to all resolvers as their third parameter. Context is the right place to do things which are shared by multiple resolvers, like user identification.
+
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), process.env.JWT_SECRET
+      )
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 });
 
 server.listen().then(({ url }) => {
